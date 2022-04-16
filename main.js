badc82bdff29dd32a820bdbc77ba2065598574ea
yuyeon329 <peachberryjr@gmail.com>
2022-04-16 21:53:29 +0900

    add random image function

---
+++ main.js
@@ -0,0 +1,13 @@
+// 랜덤하게 바뀌어서 유저가 누르길 기다리는 가위바위보 이미지
+let imgArray = new Array();
+imgArray[0] = "rock.png";
+imgArray[1] = "scissor.png";
+imgArray[2] = "paper.png";
+
+function showImage() {
+  let imgNum = Math.round(Math.random()*2);
+  let orgImg = document.getElementById("randomimg");
+  orgImg.src = imgArray[imgNum];
+}
+
+showImage();
